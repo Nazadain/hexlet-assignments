8e481bc0ca3f6cd2176b53f628ab0be3a3d1cabd
@hexlet/cli <support@hexlet.io>
2024-12-24 13:37:54 +0500

    submit java-spring-boot-ru/first-start

---
+++ java-spring-boot-ru/first-start/src/main/java/exercise/Application.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// BEGIN
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Hello, world</h1>\n" + "<b>First spring-boot application</b>\n";
+    }
+}
+// END
