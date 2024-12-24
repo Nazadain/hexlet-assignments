a25816d8a5c20b3f02b8b954e6095a0c1ff42579
@hexlet/cli <support@hexlet.io>
2024-12-24 15:53:02 +0500

    submit java-spring-boot-ru/first-start

--- java-spring-boot-ru/first-start/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/first-start/src/main/java/exercise/Application.java
@@ -15,7 +15,7 @@ public class Application {
 
     @GetMapping("/")
     public String home() {
-        return "Hello, world!";
+        return "Hello, world! Welcome to Hexlet!";
     }
 
     @GetMapping("/about")
